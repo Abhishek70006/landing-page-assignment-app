e8aafc6d647ea8b641064b933a906e14d18f0a7f
Abhishek70006 <dayalabhi2018.in>
2025-11-18 13:22:45 +0530

    something

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,5 @@
-import axios from 'axios';
-const BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+import axios from "axios";
+const BASE =
+  process.env.REACT_APP_API_URL ||
+  "https://landing-page-assignment-app.onrender.com/api";
 export default axios.create({ baseURL: BASE });
